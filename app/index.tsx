5b379cf8b86f243d2c4e114218a1807edc638990
Maria Gabriela <gabriela.florentino@academico.ifpb.edu.br>
2026-03-17 11:28:49 -0300

    Teste TextInput

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  const [nome, setNome]= useState('');
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    <TextInput
+    placeholder="Digite algo..."
+    onChangeText={ novoTexto => setNome(novoTexto) }
+    value ={nome}
+    style={{
+        width: "100%",
+        padding: 5,
+        borderWidth:1
+    }}
+    />
+    <Text>{nome}</Text>
+    </View>
+  );
+}
